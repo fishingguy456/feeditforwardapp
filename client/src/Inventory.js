ec2d5686db9c8a30a2a507bb610b5768d96016e6
fishingguy456 <qukevin99@gmail.com>
2022-04-12 01:18:52 -0400

    asdf

--- client/src/Inventory.js
+++ client/src/Inventory.js
@@ -10,22 +10,23 @@ function Inventory(props) {
     const [newItemName, setNewItemName] = useState("");
     const [newQuantity, setNewQuantity] = useState(0);
     let result = props.results === "333" || props.results === "36663" ? "" : props.results;
+    const port = process.env.PORT || 8080;
     
     useEffect(() => {
-        Axios.get("http://localhost:3001/read").then((response) => { //promise
+        Axios.get(`http://localhost:${port}/read`).then((response) => { //promise
             console.log(props.results);
             setbarCode(result);
             setItemList(response.data);
         });
-    }, [props.results, result]);
+    }, [props.results, result, port]);
     const addItem = () => {
         console.log(itemList);
         if(itemList.filter(e => e.barCode === barCode).length > 0 || itemList.filter(e => e.itemName === itemName).length > 0){
             alert("Item already exists");
             return;
         }
-        Axios.post("http://localhost:3001/create", {barCode: barCode, itemName: itemName, quantity: quantity,}).then(() => {
-            Axios.get("http://localhost:3001/getLatestId").then((response) => {
+        Axios.post(`http://localhost:${port}/create`, {barCode: barCode, itemName: itemName, quantity: quantity,}).then(() => {
+            Axios.get(`http://localhost:${port}/getLatestId`).then((response) => {
                 setItemList(itemList.concat({_id: response.data[0]._id, barCode: barCode, itemName: itemName, quantity: quantity}));
                 setItemName("");
                 setQuantity("");
@@ -35,7 +36,7 @@ function Inventory(props) {
         console.log("Item added to database");
     };
     const updateItem = (id, barCode) => {
-        Axios.put("http://localhost:3001/update", {_id: id, barCode: barCode, newItemName: newItemName, newQuantity: newQuantity,}).then(() => {
+        Axios.put(`http://localhost:${port}/update`, {_id: id, barCode: barCode, newItemName: newItemName, newQuantity: newQuantity,}).then(() => {
         setItemList(itemList.map((item) => {
             if (item._id === id) {
                 item.barCode = barCode;
@@ -48,7 +49,7 @@ function Inventory(props) {
         console.log("Item updated in database");
     };
     const deleteItem = (id) => {
-        Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
+        Axios.delete(`http://localhost:${port}/delete/${id}`).then(() => {
         setItemList(itemList.filter((item) => item._id !== id));
         });
         console.log("Item deleted from database");
